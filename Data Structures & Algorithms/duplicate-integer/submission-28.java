cf5b5b8eac4428725c21f9522478078ae72ccd75
Anushkatech <104856348+Anushkatech@users.noreply.github.com>
2026-06-04 03:28:45 +0530

    Add: duplicate-integer - submission-28

---
+++ Data Structures & Algorithms/duplicate-integer/submission-28.java
@@ -0,0 +1,16 @@
+class Solution {
+        public boolean hasDuplicate(int[] nums) {
+                HashSet<Integer> hset = new HashSet<>();
+                        for(int num : nums)
+                                {
+                                            if(hset.contains(num)) {
+                                                
+                                                                        return true;
+                                                                                    }
+                                                                                                hset.add(num);
+                                                                                                        }
+                                                                                                                return false;
+                                                                                                                        
+                                                                                                                            
+                                                                                                                            }
+}
